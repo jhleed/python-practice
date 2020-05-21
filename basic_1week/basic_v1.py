57001ec5bb26e9b9728ad24cfe8d552532b02a6e
이종호 <jhleed3@gmail.com>
2020-05-21 16:21:09 +0900

    print

--- basic_1week/basic_v1.py
+++ basic_1week/basic_v1.py
@@ -3,3 +3,7 @@ print("Hello world" + " Thank you")
 print(900629)
 
 print("%s 입니다." % "홍길동")
+print("%s 입니다." % "이순신")
+print("%s 원 입니다." % 5000)
+
+print("이름은 %s 이고 %s 에 삽니다." % ("홍길동", "서울"))  # %의 개수에 맞게 인자를 줘야 한다.
