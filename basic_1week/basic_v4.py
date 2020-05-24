5a5ddc3b229bef794883b81047b0de27bbd726c5
이종호 <jhleed3@gmail.com>
2020-05-24 11:42:55 +0900

    method

---
+++ basic_1week/basic_v4.py
@@ -0,0 +1,23 @@
+def english():
+    print("영어과입니다.")
+
+
+english()
+
+
+def match(student_name):
+    print(student_name)
+
+
+match("이종호")
+
+
+def call_function(func):
+    func()
+
+
+def help():
+    print("도와주러 왔습니다.")
+
+
+call_function(help)
